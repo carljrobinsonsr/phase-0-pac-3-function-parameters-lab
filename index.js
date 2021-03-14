514d3d27c2d833421b4deb02b6ab4b0ecdb2c383
carljrobinsonsr <carljrobinsonsr@gmail.com>
2021-03-13 21:01:08 -0500

    1st commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function introduction(name) {
+    return ("Hi, my name is " +name + ".")}
+    console.log(introduction('Josh'))  
+  
+    {
+        language = Javascript;
+      }
+
+    function introductionWithLanguage (name, language ) {
+        return ("Hi, my name is "+name+" and I am learning to program in "+language+".")}
+        console.log(introductionWithLanguage ('Josh', 'language')) 
+
+        function introductionWithLanguageOptional (name, language ) {
+            return ("Hi, my name is "+name+" and I am learning to program in "+language+".")
+            if (language === undefined) {
+                language = Javascript;} 
+            console.log(introductionWithLanguageOptional ('Josh', 'Javascript')) 
+
+           
+
+            
